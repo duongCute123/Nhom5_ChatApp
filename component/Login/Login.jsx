0d8727267cf5c2378557efbbfb0cd1f7e39400a9
duongCute123 <89206817+duongCute123@users.noreply.github.com>
2022-11-02 19:14:57 +0700

    add sigup

---
+++ component/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Component } from "react";
+import { View,Text,TextInput,Image,Button } from "react-native";
+import { useState,useEffect,useContext,useRef } from "react";
+const Login=()=>{
+    return(
+        <View>
+            <Text>Hello các bạn nhá</Text>
+        </View>
+    )
+}
+export default Login
---
+++ database/firebase.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+import { Component } from "react";
+import firebase from "firebase"
+const firebaseConfig = {
+    apiKey: "AIzaSyDGARW6u5a0SpboIuDlgLZ7aGR-nTanWx0",
+    authDomain: "loginreactnative-e75d1.firebaseapp.com",
+    projectId: "loginreactnative-e75d1",
+    storageBucket: "loginreactnative-e75d1.appspot.com",
+    messagingSenderId: "200542830914",
+    appId: "1:200542830914:web:78bdcea0a38241ac19c8db",
+    measurementId: "G-88SXVKMDHQ"
+};
+firebase.initializeApp(firebaseConfig);
+export default firebase;
